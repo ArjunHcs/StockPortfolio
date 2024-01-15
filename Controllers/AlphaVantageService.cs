980f7a64846ab93e5054fa14e5176b982461d390
Arjun <128473547+ArjunHcs@users.noreply.github.com>
2024-01-15 23:55:52 +0100

    Fixed formatting

--- Controllers/AlphaVantageService.cs
+++ Controllers/AlphaVantageService.cs
@@ -1,8 +1,25 @@
-namespace StockPortfolio.Controllers{
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+
+namespace StockPortfolio.Controllers {
 
   public class AlphaVantageService {
+    private const API_KEY = "API_KEY_HERE";
+    private readonly HttpClient httpClient;
     public AlphaVantageService(){
-        
+      httpClient = new HttpClient();
+    }
+    public async Task<Stock> GetStockInfo(string symbol){
+      string QUERY_URL = $"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={API_KEY}";
+      Uri queryUri = new Uri(QUERY_URL);
+
+      using (httpClient) {
+        dynamic json_data = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(httpClient.DownloadString(queryUri));
+      }
     }
   }
 }
--- Models/Portfolio.cs
+++ Models/Portfolio.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using StockPortfolio.Controllers;
 
 namespace StockPortfolio.Models {
 
